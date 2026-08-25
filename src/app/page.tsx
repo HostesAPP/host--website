b10dc88dd660b16d622e633c07f89e9a768729a8
larrystone <olanrewajuakinwalire@gmail.com>
2026-08-25 01:53:56 +0100

    Implemnted Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/wrapper/Footer";
 import Navbar from "@/components/wrapper/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
