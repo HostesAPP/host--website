76fa92739c59cf3c2037fbe83dbef8c005761817
larrystone <olanrewajuakinwalire@gmail.com>
2026-09-04 04:45:30 +0100

    Adjusted the Input height

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,7 +6,7 @@ type InputProps = ComponentProps<"input">;
 export default function Input({ ...props }: InputProps) {
   return (
     <div className="input-container flex items-center bg-white/20 w-screen md:w-150 md:py-4 px-5 rounded-[50px] md:h-20 h-15 border border-gray-300">
-      <input {...props} className="outline-none w-full" />
+      <input {...props} className="outline-none w-full h-full" />
       <FaChevronRight color="#EF5A22" />
     </div>
   );
