8f30ac18e485be7a2192ba4dbcdee0ec0d2b54f4
larrystone <olanrewajuakinwalire@gmail.com>
2026-09-15 23:54:51 +0100

    Adjusted the input width

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ type InputProps = ComponentProps<"input">;
 
 export default function Input({ ...props }: InputProps) {
   return (
-    <div className="input-container flex items-center bg-white/20 w-150 md:py-4 px-5 rounded-[50px] md:h-20 h-15 border border-gray-300">
+    <div className="input-container flex items-center bg-background w-full md:w-150 md:py-4 px-5 rounded-[50px] md:h-20 h-15 border border-gray-300">
       <input {...props} className="outline-none w-full h-full" />
     </div>
   );
