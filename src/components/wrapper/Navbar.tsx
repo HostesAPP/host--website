efc9850ed84d6a3c490f6f1f0ac7550b54cbc10f
larrystone <olanrewajuakinwalire@gmail.com>
2026-09-15 20:19:08 +0100

    Added padding to the nav container to avoid touching the screen

--- src/components/wrapper/Navbar.tsx
+++ src/components/wrapper/Navbar.tsx
@@ -26,84 +26,88 @@ export default function Navbar() {
 
   return (
     <nav className="bg-transparent z-100 sticky top-0">
-      <div className="nav-container mt-5 z-100 py-4 px-6 flex md:justify-center w-full md:w-fit mx-auto bg-white rounded-[50px] shadow-[inset_0_4px_20px_-12px_rgba(0,0,0,0.15),inset_0_-4px_20px_-12px_rgba(0,0,0,0.15)]">
-        <div className="flex items-center justify-between md:space-x-18 w-full">
-          <div className="hamburger-menu md:hidden">
-            <button className="cursor-pointer" onClick={() => setIsOpen(true)}>
-              <GiHamburgerMenu className="dark:text-black" />
-            </button>
-          </div>
+      <div className="px-4 md:px-8">
+        <div className="nav-container mt-5 mx-auto z-100 flex w-full max-w-full py-4 px-6 md:w-fit md:justify-center bg-white rounded-[50px] shadow-[inset_0_4px_20px_-12px_rgba(0,0,0,0.15),inset_0_-4px_20px_-12px_rgba(0,0,0,0.15)]">
+          <div className="flex items-center justify-between md:space-x-18 w-full">
+            <div className="hamburger-menu md:hidden">
+              <button className="cursor-pointer" onClick={() => setIsOpen(true)}>
+                <GiHamburgerMenu className="dark:text-black" />
+              </button>
+            </div>
 
-          <div className="image-container ml-auto">
-            <Link
-              href="/"
-              className="relative md:block md:h-6.75 md:w-25.25 h-5 w-18.25 flex items-center justify-center mb-2"
+            <div className="image-container ml-auto">
+              <Link
+                href="/"
+                className="relative md:block md:h-6.75 md:w-25.25 h-5 w-18.25 flex items-center justify-center mb-2"
+              >
+                <Image
+                  src="/images/logo.png"
+                  fill
+                  alt="Logo Image"
+                  className="object-cover"
+                />
+              </Link>
+            </div>
+            <div
+              onClick={closeMenu}
+              className={`fixed inset-0 z-40 bg-black/40 transition-opacity duration-300 md:hidden ${
+                isOpen
+                  ? "pointer-events-auto opacity-100"
+                  : "pointer-events-none opacity-0"
+              }`}
+            />
+            <div
+              className={`${mobilestyling} mobile-nav bg-white z-50 absolute md:hidden bg-neutral w-[75%] h-screen top-0 left-0 py-20 px-8 shadow-2xl transition-all duration-1000`}
             >
-              <Image
-                src="/images/logo.png"
-                fill
-                alt="Logo Image"
-                className="object-cover"
-              />
-            </Link>
-          </div>
-          <div
-            onClick={closeMenu}
-            className={`fixed inset-0 z-40 bg-black/40 transition-opacity duration-300 md:hidden ${
-              isOpen
-                ? "pointer-events-auto opacity-100"
-                : "pointer-events-none opacity-0"
-            }`}
-          />
-          <div
-            className={`${mobilestyling} mobile-nav bg-white z-50 absolute md:hidden bg-neutral w-[75%] h-screen top-0 left-0 py-20 px-8 shadow-2xl transition-all duration-1000`}
-          >
-            <div className="mobile-nav-container">
-              <div className="top-heading flex justify-between items-center mb-10">
-                <Link href="/" className="relative block h-6.75 w-25.25">
-                  <Image
-                    src="/images/logo.png"
-                    fill
-                    alt="Logo Image"
-                    className="object-contain"
-                  />
-                </Link>
-                <button className="cursor-pointer" onClick={closeMenu}>
-                  <IoCloseSharp className="dark:text-black" />
-                </button>
-              </div>
-              <div className="nav-content flex flex-col gap-6">
-                {navlinks.map(({ id, name, href }) => (
-                  <Link
-                    className="link-content flex gap-3 items-center font-extralight w-fit"
-                    key={id}
-                    href={href}
-                    onClick={closeMenu}
-                  >
-                    <span className="text-[14px] dark:text-black">{name}</span>
+              <div className="mobile-nav-container">
+                <div className="top-heading flex justify-between items-center mb-10">
+                  <Link href="/" className="relative block h-6.75 w-25.25">
+                    <Image
+                      src="/images/logo.png"
+                      fill
+                      alt="Logo Image"
+                      className="object-contain"
+                    />
                   </Link>
-                ))}
+                  <button className="cursor-pointer" onClick={closeMenu}>
+                    <IoCloseSharp className="dark:text-black" />
+                  </button>
+                </div>
+                <div className="nav-content flex flex-col gap-6">
+                  {navlinks.map(({ id, name, href }) => (
+                    <Link
+                      className="link-content flex gap-3 items-center font-extralight w-fit"
+                      key={id}
+                      href={href}
+                      onClick={closeMenu}
+                    >
+                      <span className="text-[14px] dark:text-black">
+                        {name}
+                      </span>
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="navlinks font-primary md:flex items-center gap-8 hidden">
-            {navlinks.slice(0, 3).map(({ name, href, id }) => (
+            <div className="navlinks font-primary md:flex items-center gap-8 hidden">
+              {navlinks.slice(0, 3).map(({ name, href, id }) => (
+                <Link
+                  key={id}
+                  href={href}
+                  className={`font-medium text-[14px] dark:text-black leading-5 tracking-[0.28px] ${pathname === href ? "text-primary border-b border-primary" : ""}`}
+                >
+                  {name}
+                </Link>
+              ))}
+            </div>
+            <div className="get-started hidden md:block">
               <Link
-                key={id}
-                href={href}
-                className={`font-medium text-[14px] dark:text-black leading-5 tracking-[0.28px] ${pathname === href ? "text-primary border-b border-primary" : ""}`}
+                href={"/sign-up"}
+                className="bg-primary flex items-center justify-center gap-2 text-white h-[42.66px] px-10.5 rounded-4xl"
               >
-                {name}
+                <span>Get Started</span> <FaArrowRightLong />
               </Link>
-            ))}
-          </div>
-          <div className="get-started hidden md:block">
-            <Link
-              href={"/sign-up"}
-              className="bg-primary flex items-center justify-center gap-2 text-white h-[42.66px] px-10.5 rounded-4xl"
-            >
-              <span>Get Started</span> <FaArrowRightLong />
-            </Link>
+            </div>
           </div>
         </div>
       </div>
